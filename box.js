48596c8ac9a4f647577b149f9837dbfd0006020f
sudiksha1 <70843531+sudiksha1@users.noreply.github.com>
2020-12-26 10:47:20 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,31 @@
+class Box{
+    constructor(x,y,color){
+        var options={
+            restitution :0.4,
+            friction :0.0
+        }
+        this.body=Bodies.rectangle(x,y,30,40,options);
+        this.color=color;
+        World.add(world,this.body);
+        this.visibility=255;
+    }
+    display(){
+        if(this.body.speed <5){
+            var angle=this.body.angle;
+            var pos=this.body.position;
+            fill(this.color);
+            push();
+            translate(pos.x,pos.y);
+            rotate(angle);
+            rectMode(CENTER);
+            rect(0,0,30,40);
+            pop();
+        }
+        else{
+            World.remove(world,this.body);
+            push();
+            this.visibility = this.visibility -5;
+            pop();
+        }
+    }
+}
